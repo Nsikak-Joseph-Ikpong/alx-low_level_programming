9bc5edc2f585c1e620f882ff355b0f8780f55756
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-09-07 02:31:15 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main -A program to write a line without printf and puts command
- * Return: 0 (Successful)
+ * Return: 1 (Successful)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
