44b6354589230b8d7a6eb204bc5eb573b9f0787d
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-09-07 02:20:54 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -A program to write a line without printf and puts command
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
