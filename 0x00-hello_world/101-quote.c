da48560defcb591525193bb67b8a5034624d553a
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-09-07 02:39:06 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main -A program to write a line without printf and puts command
@@ -6,6 +5,7 @@
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quo, 59);
 return (1);
 }
