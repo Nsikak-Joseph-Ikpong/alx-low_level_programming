7509edf91c207bc539e39281c6ce20689aeb8725
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-09-14 15:26:38 -0700

    hh

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,10 +9,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar(' ');
+		_putchar('-');
 		num = -num;
 	}
+
 	if ((num / 10) > 0)
 		print_number(num / 10);
+
 	_putchar((num % 10) + '0');
 }
