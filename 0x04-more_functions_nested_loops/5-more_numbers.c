87d6d5f3ee80362144c5355ad1d8d9616b390cb6
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-09-14 09:15:00 -0700

    momentum

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - A program to print numbers
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int z;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (z = 0 ; z <= 14 ; z++)
+		{
+			if (z > 9)
+				_putchar(z / 10 + '0');
+			_putchar(z % 10 + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+
