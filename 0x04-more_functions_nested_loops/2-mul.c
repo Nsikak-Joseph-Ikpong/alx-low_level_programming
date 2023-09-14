96b53002645f5f684c9059633fdf4a331eb16be8
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-09-14 08:22:29 -0700

    This is the best keyboard

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul -A program to multiply two in puts
+ * @a: function parameter
+ * @b: functionb parameter
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
