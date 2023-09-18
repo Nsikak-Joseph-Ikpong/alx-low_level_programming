50742bfbaa4cddac4d409a0ac07a35413e4fdb61
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-09-18 00:36:05 -0700

    manny

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - A program to swap two integers
+ * @a: function parameter
+ * @b: function parameter
+ */
+void swap_int(int *a, int *b);
+{
+	int n;
+
+	n = *a;
+	*a = *b;
+	*b = m;
+}
