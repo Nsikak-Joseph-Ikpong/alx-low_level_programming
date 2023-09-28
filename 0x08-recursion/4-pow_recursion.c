140d4d7826476ad1b64fe1ab13428c4886ddbe39
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-09-28 14:35:35 -0700

    hhh

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  * _pow_recursion - returns the value of x raised to the power of yt
  * @x: valkue to be raised
  * @y: power
- * Returne: result of power
+ * Return: result of power
  */
 int _pow_recursion(int x, int y)
 {
