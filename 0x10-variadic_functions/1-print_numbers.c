5564b5bd44b12e93d8af131a6ee8f8545ded1dc5
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-10-12 12:12:49 -0700

    kkk

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - A functions that prints numbers
+ * @separator: string to be printed
+ * @n: numbers of integers to be passed to the function
+ * @...: A variable number of numbers to be printed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int comm;
+
+	va_start(ap, n);
+
+	for (comm = 0; comm < n; comm++)
+	{
+		printf("%d", va_arg(ap, int));
+
+		if (comm != (n - 1) && seperator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
+
