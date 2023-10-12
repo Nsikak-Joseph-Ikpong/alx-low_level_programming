3e2ddc1eb9ff60bc40e1ba391fa9bea4699431d1
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-10-12 12:53:05 -0700

    jjj

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
-		if (index != (n - 1) && searator != NULL)
+		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
