e954d61ff2f309b509dbfd4539859111067269ff
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-10-12 12:14:35 -0700

    kkk

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ap, int));
 
-		if (comm != (n - 1) && seperator != NULL)
+		if (comm != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
