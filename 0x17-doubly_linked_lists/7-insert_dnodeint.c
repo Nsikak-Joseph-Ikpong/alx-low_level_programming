6566b4e319c7ba8962e23a37d07fd9db1444ee73
Nsikak-Joseph-Ikpong <ikpongnsikak88@gmail.com>
2023-12-07 13:40:46 -0800

    Update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,36 +12,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	if (h == NULL)
-		 return (NULL);
+	dlistint_t *tmp = *h, *new;
 
-	dlistint_t *current = *h, *new_node;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
 
-	 if (idx == 0)
-		 return (add_dnodeint(h, n));
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-	 for (; idx != 1; idx--)
-	 {
-		 if (current == NULL)
-			  return (NULL);
-		 current = current->next;
-	 }
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-	 new_node = malloc(sizeof(dlistint_t));
-	 if (new_node == NULL)
-		 return (NULL);
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
-
-	 new_node->n = n;
-	 new_node->prev = current;
-	 new_node->next = (current != NULL) ? current->next : NULL;
-
-	 if (current != NULL)
-		 current->next = new_node;
-
-	  if (new_node->next != NULL)
-		  new_node->next->prev = new_node;
-
-	  return (new_node);
+	return (new);
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,32 +11,31 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	 dlistint_t *current = *head;
+	dlistint_t *current = *head;
 
+	if (*head == NULL)
+		return (-1);
 
-	 if (*head == NULL)
-		 return (-1);
+	for (; index != 0; index--)
+	{
+		if (current == NULL)
+			return (-1);
+		current = current->next;
+	}
 
-	 for (; index != 0; index--)
-	 {
-		 if (current == NULL)
-			 return (-1);
-		 current = current->next;
-	 }
+	if (current == *head)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		current->prev->next = current->next;
+		if (current->next != NULL)
+			current->next->prev = current->prev;
+	}
 
-	 if (current == *head)
-	 {
-		 *head = current->next;
-		 if (*head != NULL)
-			 (*head)->prev = NULL;
-	 }
-	 else
-	 {
-		 current->prev->next = current->next;
-		 if (current->next != NULL)
-			 current->next->prev = current->prev;
-	 }
-
-	 free(current);
-	 return (1);
+	free(current);
+	return (1);
 }
